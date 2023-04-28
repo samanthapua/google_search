2e7d3eca4a772ef25f3794f9d5cef98348587237
samanthapua <52142776+samanthapua@users.noreply.github.com>
2023-04-28 23:49:24 +0800

    New project to track google search interest and btc prices
    Completed:
    - Extract data from Google Search PyTrends API
    - Import BTC prices

---
+++ main.py
@@ -0,0 +1,22 @@
+from pytrends.request import TrendReq
+import pandas as pd
+
+btc_prices = pd.read_csv('./historical_btc_prices.csv',usecols=['Date','Close','Volume'])
+
+def settings(lang,timezone,search_words,timeperiod):
+    pytrends = TrendReq(hl=lang, tz=timezone)
+    pytrends.build_payload(search_words,cat=0,timeframe=timeperiod)
+    count_df = pytrends.interest_over_time()
+    country_df = pytrends.interest_by_region(resolution='COUNTRY',inc_low_vol=True,inc_geo_code=False)
+    related_query_df = pytrends.related_queries()
+    return count_df, country_df, related_query_df
+
+if __name__ == '__main__':
+    count_df,country_df,related_query_df = settings('en-US',360,['Bitcoin','BTC'],'today 5-y')
+    count_df = count_df.reset_index()
+    count_df.loc[:, 'average_interest'] = (count_df['Bitcoin'] + count_df['BTC'])/2
+    count_df['date'] = pd.to_datetime(count_df['date'])
+    count_df = count_df[['date', 'average_interest']]
+    btc_prices['Date'] = pd.to_datetime(btc_prices['Date'])
+    df = pd.merge(count_df, btc_prices, left_on='date', right_on='Date',how='inner')
+
